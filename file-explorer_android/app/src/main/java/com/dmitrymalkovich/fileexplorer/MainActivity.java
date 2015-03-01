7c46d0e625b82a73c2f45c20a50c60a3a4554e8b
Dmitry Malkovich <dima.malkovich@gmail.com>
2015-03-01 20:20:47 +0300

    Configured j2objc-gradle-plugin and converted example class to objc

---
+++ file-explorer_android/app/src/main/java/com/dmitrymalkovich/fileexplorer/MainActivity.java
@@ -0,0 +1,9 @@
+package com.dmitrymalkovich.fileexplorer;
+
+/**
+ * FileExplorer
+ * Created by Dmitry Malkovich on 3/1/15.
+ * Copyright (c) 2015 Dmitry Malkovich. All rights reserved.
+ */
+public class MainActivity {
+}
---
+++ file-explorer_android/app/src/main/java/engine/Java2ObjcExampleClass.java
@@ -0,0 +1,14 @@
+package engine;
+
+/**
+ * File Explorer
+ * Created by Dmitry Malkovich on 3/1/15.
+ * Copyright (c) 2015 Dmitry Malkovich. All rights reserved.
+ */
+public class Java2ObjcExampleClass {
+
+    public Java2ObjcExampleClass() {
+        System.out.println("Hello world");
+    }
+
+}
